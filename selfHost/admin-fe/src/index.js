ba325b7e62c082b28c184699d1150eca39e8ff4f
Tung Nguyen <tung.nguyenthanh@shopee.com>
2020-02-19 20:56:36 +0700

    Update

--- selfHost/admin-fe/src/index.js
+++ selfHost/admin-fe/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import App from 'App'
 import * as serviceWorker from 'serviceWorker'
@@ -8,9 +8,9 @@ import createStore from 'store/createStore'
 
 const app = (
   <Provider store={createStore()}>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </Provider>
 )
 
